8f3045c6a6002d1e50095e37a58ef6b75fbd7960
akshay <akshay@MBAir.lan>
2023-03-10 20:25:19 -0800

    added template files and updated driver code

--- README.md
+++ README.md
@@ -19,16 +19,6 @@ python3 -m venv venv --system-site-packages
 source venv/bin/activate
 deactivate
 
-# Assignment # 1:
+# Step # 1:
 
-Moving further with this course, it will be required to work on Flask framework and MySQL database. For that, you will need Python, Flask and MySQL installed on your computer. Your task is to set up Python, Flask and install MySql workbench or any similar mysql database editor. If you have your setup ready, post a REPLY with 'I am ready with my setup' or post your question if you have any regarding setting up the required softwares and framework.
-
-You can test your Flask setup by running any of the flask code given in the slides "Getting started with Flask".
-Steps:
-
-Install python and flask.
-
-1. Copy paste the code in a text file and save it with .py extension.
-2. From cmd or terminal go to the folder in which file is saved and run the python file using python commands.
-
-Make sure you see your code working and displaying content on the browser before posting a reply 'I am ready with my setup'.
+Install python, flask, mysql.
--- app.py
+++ app.py
@@ -1,22 +1,45 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template, request, make_response
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return "This is the home page <h1> You are Welcome! </h1>"
+# @app.route("/")
+# def home():
+#     return "This is the home page <h1> You are Welcome! </h1>"
+
+# @app.route("/<name>")
+# def user(name):
+#     return f"Hello {name}!"
 
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
+# @app.route("/admin")
+# def admin():
+#     # return redirect(url_for("home"))
+#     return redirect(url_for("user", name="Bob_admin"))
+
+# @app.route("/<name>")
+# def webpage(name):
+#     # return redirect(url_for("home"))
+#     return render_template("index.html", content=name)
+@app.route('/')
+def index():
+    return render_template('cookies.html')
 
 
-@app.route("/admin")
-def admin():
-    # return redirect(url_for("home"))
-    return redirect(url_for("user", name="Admin!"))
+@app.route('/setcookie', methods=['POST', 'GET'])
+def setcookie():
+    if request.method == 'POST':
+        user = request.form['nm']
+        resp = make_response(render_template('readcookies.html'))
+        resp.set_cookie('userID', user)
+        return resp
+
+
+@app.route('/getcookie')
+def getcookie():
+    name = request.cookies.get('userID')
+    print(name)
+    return '<h1>welcome ' + name + '</h1>'
 
 
 if __name__ == "__main__":
