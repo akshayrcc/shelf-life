3473f1c84f6cc86a6fe3fe89b5dd0912f2ebe5ee
akshay <akshay@MBAir.lan>
2023-03-10 19:59:09 -0800

    tested different endpoints

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
-app = Flask(__main__)
+app = Flask(__name__)
 
-@app.route()
+
+@app.route("/")
 def home():
-    return "This is the home page"
+    return "This is the home page <h1> You are Welcome! </h1>"
+
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+
+@app.route("/admin")
+def admin():
+    # return redirect(url_for("home"))
+    return redirect(url_for("user", name="Admin!"))
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5500, debug=True)
