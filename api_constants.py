8def416619404473c71e42771d0e7e87a8947a39
Akshay RC <er.akshayrc@gmail.com>
2023-05-15 16:24:07 -0700

    hused pass and commited

--- api_constants.py
+++ api_constants.py
@@ -1,6 +1,7 @@
 mongdb_username = ""
 mongdb_dbname = "API"
 mongodb_pass = ""
+mongodb_hostname = ""
 secret_key = "mysecretkey"
 UPLOAD_FOLDER = '/Users/akshay/Documents/uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
